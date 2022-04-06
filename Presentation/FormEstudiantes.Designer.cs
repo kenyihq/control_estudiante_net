2fa7a01fe2164a535a885a99343785bbcd9db17f
Kenyi Hancco <kenyihq@gmail.com>
2022-04-06 06:52:08 -0500

    Se termino el proyecto, muchas cosas se pueden mejorar, pero todo corre correctamente

--- Presentation/FormEstudiantes.Designer.cs
+++ Presentation/FormEstudiantes.Designer.cs
@@ -85,10 +85,11 @@
             // 
             // dataEst
             // 
+            this.dataEst.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.dataEst.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataEst.Location = new System.Drawing.Point(12, 42);
             this.dataEst.Name = "dataEst";
-            this.dataEst.Size = new System.Drawing.Size(475, 434);
+            this.dataEst.Size = new System.Drawing.Size(1147, 434);
             this.dataEst.TabIndex = 2;
             this.dataEst.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataEst_CellContentClick);
             // 
@@ -96,7 +97,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(493, 42);
+            this.label2.Location = new System.Drawing.Point(1161, 40);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(125, 20);
             this.label2.TabIndex = 3;
@@ -106,7 +107,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(493, 88);
+            this.label3.Location = new System.Drawing.Point(1161, 86);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(146, 20);
             this.label3.TabIndex = 4;
@@ -116,7 +117,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(493, 134);
+            this.label4.Location = new System.Drawing.Point(1161, 132);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(127, 20);
             this.label4.TabIndex = 5;
@@ -124,28 +125,28 @@
             // 
             // pNombre
             // 
-            this.pNombre.Location = new System.Drawing.Point(497, 65);
+            this.pNombre.Location = new System.Drawing.Point(1165, 63);
             this.pNombre.Name = "pNombre";
             this.pNombre.Size = new System.Drawing.Size(279, 20);
             this.pNombre.TabIndex = 6;
             // 
             // sNombre
             // 
-            this.sNombre.Location = new System.Drawing.Point(497, 111);
+            this.sNombre.Location = new System.Drawing.Point(1165, 109);
             this.sNombre.Name = "sNombre";
             this.sNombre.Size = new System.Drawing.Size(279, 20);
             this.sNombre.TabIndex = 7;
             // 
             // pApellido
             // 
-            this.pApellido.Location = new System.Drawing.Point(497, 157);
+            this.pApellido.Location = new System.Drawing.Point(1165, 155);
             this.pApellido.Name = "pApellido";
             this.pApellido.Size = new System.Drawing.Size(279, 20);
             this.pApellido.TabIndex = 8;
             // 
             // sApellido
             // 
-            this.sApellido.Location = new System.Drawing.Point(497, 203);
+            this.sApellido.Location = new System.Drawing.Point(1165, 201);
             this.sApellido.Name = "sApellido";
             this.sApellido.Size = new System.Drawing.Size(279, 20);
             this.sApellido.TabIndex = 10;
@@ -154,7 +155,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(493, 180);
+            this.label5.Location = new System.Drawing.Point(1161, 178);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(148, 20);
             this.label5.TabIndex = 9;
@@ -162,7 +163,7 @@
             // 
             // tel
             // 
-            this.tel.Location = new System.Drawing.Point(497, 249);
+            this.tel.Location = new System.Drawing.Point(1165, 247);
             this.tel.Name = "tel";
             this.tel.Size = new System.Drawing.Size(279, 20);
             this.tel.TabIndex = 12;
@@ -171,7 +172,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(493, 226);
+            this.label6.Location = new System.Drawing.Point(1161, 224);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(79, 20);
             this.label6.TabIndex = 11;
@@ -179,7 +180,7 @@
             // 
             // cel
             // 
-            this.cel.Location = new System.Drawing.Point(497, 295);
+            this.cel.Location = new System.Drawing.Point(1165, 293);
             this.cel.Name = "cel";
             this.cel.Size = new System.Drawing.Size(279, 20);
             this.cel.TabIndex = 14;
@@ -188,7 +189,7 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(493, 272);
+            this.label7.Location = new System.Drawing.Point(1161, 270);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(65, 20);
             this.label7.TabIndex = 13;
@@ -196,7 +197,7 @@
             // 
             // direccion
             // 
-            this.direccion.Location = new System.Drawing.Point(497, 341);
+            this.direccion.Location = new System.Drawing.Point(1165, 339);
             this.direccion.Name = "direccion";
             this.direccion.Size = new System.Drawing.Size(279, 20);
             this.direccion.TabIndex = 16;
@@ -205,7 +206,7 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(493, 318);
+            this.label8.Location = new System.Drawing.Point(1161, 316);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(84, 20);
             this.label8.TabIndex = 15;
@@ -213,7 +214,7 @@
             // 
             // email
             // 
-            this.email.Location = new System.Drawing.Point(497, 387);
+            this.email.Location = new System.Drawing.Point(1165, 385);
             this.email.Name = "email";
             this.email.Size = new System.Drawing.Size(279, 20);
             this.email.TabIndex = 18;
@@ -222,7 +223,7 @@
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(493, 364);
+            this.label9.Location = new System.Drawing.Point(1161, 362);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(53, 20);
             this.label9.TabIndex = 17;
@@ -230,7 +231,7 @@
             // 
             // nac
             // 
-            this.nac.Location = new System.Drawing.Point(497, 433);
+            this.nac.Location = new System.Drawing.Point(1165, 431);
             this.nac.Name = "nac";
             this.nac.Size = new System.Drawing.Size(279, 20);
             this.nac.TabIndex = 20;
@@ -239,7 +240,7 @@
             // 
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(493, 410);
+            this.label10.Location = new System.Drawing.Point(1161, 408);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(98, 20);
             this.label10.TabIndex = 19;
@@ -247,7 +248,7 @@
             // 
             // obs
             // 
-            this.obs.Location = new System.Drawing.Point(497, 479);
+            this.obs.Location = new System.Drawing.Point(1165, 477);
             this.obs.Multiline = true;
             this.obs.Name = "obs";
             this.obs.Size = new System.Drawing.Size(279, 55);
@@ -257,7 +258,7 @@
             // 
             this.label11.AutoSize = true;
             this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label11.Location = new System.Drawing.Point(493, 456);
+            this.label11.Location = new System.Drawing.Point(1161, 454);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(127, 20);
             this.label11.TabIndex = 21;
@@ -270,12 +271,13 @@
             this.btnNuevo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnNuevo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnNuevo.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.btnNuevo.Location = new System.Drawing.Point(12, 498);
+            this.btnNuevo.Location = new System.Drawing.Point(181, 496);
             this.btnNuevo.Name = "btnNuevo";
             this.btnNuevo.Size = new System.Drawing.Size(116, 36);
             this.btnNuevo.TabIndex = 23;
             this.btnNuevo.Text = "Nuevo";
             this.btnNuevo.UseVisualStyleBackColor = false;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
             // 
             // btnGuardar
             // 
@@ -284,12 +286,13 @@
             this.btnGuardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnGuardar.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.btnGuardar.Location = new System.Drawing.Point(130, 498);
+            this.btnGuardar.Location = new System.Drawing.Point(422, 498);
             this.btnGuardar.Name = "btnGuardar";
             this.btnGuardar.Size = new System.Drawing.Size(116, 36);
             this.btnGuardar.TabIndex = 24;
             this.btnGuardar.Text = "Guardar";
             this.btnGuardar.UseVisualStyleBackColor = false;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
             // 
             // btnEditar
             // 
@@ -298,12 +301,13 @@
             this.btnEditar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnEditar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnEditar.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.btnEditar.Location = new System.Drawing.Point(252, 498);
+            this.btnEditar.Location = new System.Drawing.Point(682, 496);
             this.btnEditar.Name = "btnEditar";
             this.btnEditar.Size = new System.Drawing.Size(116, 36);
             this.btnEditar.TabIndex = 25;
             this.btnEditar.Text = "Editar";
             this.btnEditar.UseVisualStyleBackColor = false;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
             // 
             // btnEliminar
             // 
@@ -312,19 +316,20 @@
             this.btnEliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnEliminar.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.btnEliminar.Location = new System.Drawing.Point(371, 498);
+            this.btnEliminar.Location = new System.Drawing.Point(935, 496);
             this.btnEliminar.Name = "btnEliminar";
             this.btnEliminar.Size = new System.Drawing.Size(116, 36);
             this.btnEliminar.TabIndex = 26;
             this.btnEliminar.Text = "Eliminar";
             this.btnEliminar.UseVisualStyleBackColor = false;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
             // 
             // FormEstudiantes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
-            this.ClientSize = new System.Drawing.Size(790, 546);
+            this.ClientSize = new System.Drawing.Size(1453, 546);
             this.Controls.Add(this.btnEliminar);
             this.Controls.Add(this.btnEditar);
             this.Controls.Add(this.btnGuardar);
--- Presentation/FormEstudiantes.cs
+++ Presentation/FormEstudiantes.cs
@@ -67,5 +67,111 @@ namespace Presentation
         {
 
         }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            LimpiarCajas();
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            if (dataEst.SelectedRows.Count > 0)
+            {
+                Editar = true;
+                pNombre.Text = dataEst.CurrentRow.Cells[1].Value.ToString();
+                sNombre.Text = dataEst.CurrentRow.Cells[2].Value.ToString();
+                pApellido.Text = dataEst.CurrentRow.Cells[3].Value.ToString();
+                sApellido.Text = dataEst.CurrentRow.Cells[4].Value.ToString();
+                tel.Text = dataEst.CurrentRow.Cells[5].Value.ToString();
+                cel.Text = dataEst.CurrentRow.Cells[6].Value.ToString();
+                direccion.Text = dataEst.CurrentRow.Cells[7].Value.ToString();
+                email.Text = dataEst.CurrentRow.Cells[8].Value.ToString();
+                nac.Text = dataEst.CurrentRow.Cells[9].Value.ToString();
+                obs.Text = dataEst.CurrentRow.Cells[10].Value.ToString();
+            }
+            else
+            {
+                MensajeError("Seleccione una fila");
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (dataEst.SelectedRows.Count > 0)
+            {
+                DialogResult opcion;
+                opcion = MessageBox.Show("Realmente desea eliminar el registro", "Registro de estudiantes", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+                if (opcion == DialogResult.OK)
+                {
+                    ObjEntidad.id_est = Convert.ToInt32(dataEst.CurrentRow.Cells[0].Value.ToString());
+                    ObjNegocios.EliminarResgistros(ObjEntidad);
+
+                    MensajeConfirmacion("Se elimino correctamente el registro");
+
+                    MostrarRegistro();
+                }
+                else
+                {
+                    MensajeError("Seleccione un fila");
+                }
+            }
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (Editar == false)
+            {
+                try
+                {
+                    ObjEntidad.primer_nombre = pNombre.Text.ToUpper();
+                    ObjEntidad.segundo_nombre = sNombre.Text.ToUpper();
+                    ObjEntidad.primer_apellido = pApellido.Text.ToUpper();
+                    ObjEntidad.segundo_apellido = sApellido.Text.ToUpper();
+                    ObjEntidad.telefono = tel.Text.ToUpper();
+                    ObjEntidad.celular = cel.Text.ToUpper();
+                    ObjEntidad.direccion = direccion.Text.ToUpper();
+                    ObjEntidad.email = email.Text.ToUpper();
+                    ObjEntidad.nacimiento = nac.Text;
+                    ObjEntidad.observaciones = obs.Text;
+
+                    ObjNegocios.InsertarRegistros(ObjEntidad);
+
+                    MensajeConfirmacion("Se insertó correctamente");
+                    MostrarRegistro();
+                    LimpiarCajas();
+                }
+                catch (Exception ex)
+                {
+                    MensajeError("No se pudo ingresar el registro");
+                }
+            }
+            if (Editar == true)
+            {
+                try
+                {
+                    
+                    ObjEntidad.primer_nombre = pNombre.Text.ToUpper();
+                    ObjEntidad.segundo_nombre = sNombre.Text.ToUpper();
+                    ObjEntidad.primer_apellido = pApellido.Text.ToUpper();
+                    ObjEntidad.segundo_apellido = sApellido.Text.ToUpper();
+                    ObjEntidad.telefono = tel.Text.ToUpper();
+                    ObjEntidad.celular = cel.Text.ToUpper();
+                    ObjEntidad.direccion = direccion.Text.ToUpper();
+                    ObjEntidad.email = email.Text.ToUpper();
+                    ObjEntidad.nacimiento = nac.Text;
+                    ObjEntidad.observaciones = obs.Text;
+
+                    ObjNegocios.EditarRegsitro(ObjEntidad);
+
+                    MensajeConfirmacion("Se modificó correctamente");
+                    MostrarRegistro();
+                    LimpiarCajas();
+                }
+                catch (Exception ex)
+                {
+                    MensajeError("No se pudo modificar el registro");
+                }
+            }
+        }
     }
 }
